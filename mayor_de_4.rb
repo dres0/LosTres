a96254048b180dd63392033ef82f1d4d614793cb
dres0 <dres0@DESKTOP-RN3G9FF.localdomain>
2022-03-22 22:04:52 -0300

    Finished: mayor_de_4.rb

--- mayor_de_4.rb
+++ mayor_de_4.rb
@@ -2,12 +2,19 @@
 num1 = ARGV[0]
 num2 = ARGV[1]
 num3 = ARGV[2]
+# El cuarto numero puede venir vacío
+num4 = ARGV[3]
 
 # Comparar cual es el numero mayor
  mayor = num1
 
  mayor = num2 if mayor < num2
  mayor = num3 if mayor < num3
+ 
+ if !num4.nil?
+    num4 = num4.to_i
+    mayor = num4 if mayor < num4
+ end 
 
 # Mostrar el mayor 
  puts mayor
