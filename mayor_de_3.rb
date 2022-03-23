db4e23e35481b111d91479d7d1c8b0f42813d6a6
dres0 <dres0@DESKTOP-RN3G9FF.localdomain>
2022-03-22 21:48:44 -0300

    Finished: mayor_de_3.rb

---
+++ mayor_de_3.rb
@@ -0,0 +1,13 @@
+# El usuario debe ingresar 3 números
+num1 = ARGV[0]
+num2 = ARGV[1]
+num3 = ARGV[3]
+
+# Comparar cual es el numero mayor
+ mayor = num1
+
+ mayor = num2 if mayor < num2
+ mayor = num3 if mayor z num3
+
+# Mostrar el mayor 
+ puts mayor
